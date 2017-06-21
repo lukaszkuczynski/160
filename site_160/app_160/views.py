9666758e1d8d2232fb135c986a7622cf769d857b
kuczynskilukasz <kuczynskilukasz@gmail.com>
2017-06-21 16:02:32 +0200

    added Create Summary page

--- site_160/app_160/views.py
+++ site_160/app_160/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'app_160/index.html')
+
+def create_summary(request):
+    return render(request, 'app_160/create_summary.html')
