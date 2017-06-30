d2eab2860f464673646f9902154b0df06653091f
kuczynskilukasz <kuczynskilukasz@gmail.com>
2017-06-30 16:56:18 +0200

    Coming back to relational DB

--- site_160/app_160/models.py
+++ site_160/app_160/models.py
@@ -1,12 +1,9 @@
 from django.db import models
 
-from mongoengine import *
 
-connect('160')
-
-class SummaryModel(Document):
-    author = StringField(max_length=100)
-    url = StringField(max_length=100)
-    text = StringField(max_length=160)
-    tags = StringField(max_length=100)
+class SummaryModel(models.Model):
+    author = models.CharField(max_length=100)
+    url = models.CharField(max_length=100)
+    text = models.CharField(max_length=160)
+    tags = models.CharField(max_length=100)
 
