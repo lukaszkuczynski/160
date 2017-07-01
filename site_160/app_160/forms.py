db23049e3765b4931a3d22a6e01b6ae22566d27a
kuczynskilukasz <kuczynskilukasz@gmail.com>
2017-07-01 17:18:52 +0200

    new summary form, even improved

--- site_160/app_160/forms.py
+++ site_160/app_160/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 
 class SummaryForm(forms.Form):
-    url = forms.URLField(label='link', max_length=100, widget=forms.TextInput(attrs={'class':'form-control'}))
+    url = forms.URLField(label='link', max_length=100, widget=forms.TextInput(attrs={'placeholder':'link do oryginalego utworu'}))
     text = forms.CharField(label='tekst', max_length=160, widget=forms.Textarea(attrs={'placeholder': 'to jest miejsce na twoje 160 znaków...'}))
-    tags = forms.CharField(label='tagi', max_length=100, widget=forms.TextInput())
+    tags = forms.CharField(label='tagi', max_length=100, widget=forms.TextInput(attrs={'placeholder': 'tagi opisujące Twój wpis, przedzielone przecinkami'}))
 
 
