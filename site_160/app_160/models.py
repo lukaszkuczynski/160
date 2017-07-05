afd9bf365193bf5ce7a022a6b08618fb26296c01
kuczynskilukasz <kuczynskilukasz@gmail.com>
2017-07-05 17:52:18 +0200

    Timestamps introduced to Summary model

--- site_160/app_160/models.py
+++ site_160/app_160/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 
 class SummaryModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
     author = models.CharField(max_length=100)
     url = models.CharField(max_length=100)
     text = models.CharField(max_length=160)
